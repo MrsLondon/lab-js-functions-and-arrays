b82a60e31f58785510477513a5399b37f25d194a
ayat <ayat@ayats-MacBook-Air.fritz.box>
2024-10-22 15:00:46 +0200

    lab complete

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -39,22 +39,59 @@ console.log(findLongestWord(words));
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
-let sum = 0; 
 
-function sumNumbers() {}
+function sumNumbers(numbersArray) {
+    let sum = 0; 
+   // Iterating over the array using its length
+    for(let i = 0; i < numbersArray.length; i++){
+        sum += numbersArray[i]
+    }
+    return sum; 
+}
 
+console.log(sumNumbers(numbers))
 
 
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
+//pseudocode..first check if the array is empty return 0
+//to calculate average we divide the sum by the total number of values.. so total/8 numbers in bracet
+//since we have a code that calculate the sum it would be better to implement it in this function .. how?
 
-function averageNumbers() {}
-
+function averageNumbers(arrayOfNumbers) {
+    if(arrayOfNumbers.length === 0){
+        return 0
+    }
+   //to emplement sumNumbers function to calculate the sum of arrayOfNumbers we use a varialble
+   const totalSum = sumNumbers(arrayOfNumbers);
+   //another variable to store the avrage
+   const average = totalSum /arrayOfNumbers.length
+   return average; 
+}
+console.log(averageNumbers(numbers2))
 
 
 
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(wordsArray, wordToFind) {
+    if (wordsArray.length === 0) {
+        return null;
+    }
+    // Loop through the array
+    for (let i = 0; i < wordsArray.length; i++) {
+        // Check if the current word matches the word we're looking for
+        if (wordsArray[i] === wordToFind) {
+            return true; //should return true if the word we are looking for is in the array
+        }
+    }
+
+    return false; //should return false if word is not found
+}
+
+
+console.log(doesWordExist(words2, "subset")); 
+console.log(doesWordExist(words2, "apple")); 
+console.log(doesWordExist([], "truth")); 
